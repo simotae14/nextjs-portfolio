6c171c6f35c2ba3aba7f2f4872b810752b2018e1
Simone Taeggi <startae14@gmail.com>
2023-02-20 23:28:30 +0100

    Navigation in next.js

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>contact</h1>
+      <Link href="/">home</Link>
+    </div>
+  );
 }
