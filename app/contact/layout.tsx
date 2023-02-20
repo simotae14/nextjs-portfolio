cfed470b45d2bd6997f81d7c06e29179a573a3c2
Simone Taeggi <startae14@gmail.com>
2023-02-20 22:48:27 +0100

    head component and layout, root layout and nested layout

---
+++ app/contact/layout.tsx
@@ -0,0 +1,12 @@
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      <div>contact layout</div>
+      <div>{ children }</div>
+    </div>
+  )
+}
